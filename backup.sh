1a0875583d90647cedfda1d3085c6e9a9c63cf67
Phill Coxon <phill@getresults.co.nz>
2019-08-15 09:46:36 +1200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -9,6 +9,6 @@ wp cron event run wp_update_plugins # check for new plugins
 wp plugin update --all
 wp plugin install wpcore --activate
 echo "y" | wp wpcore install --key=AedB0Pfzo1yVYcxZZVzI --activate=yes 
-wp plugin activate all-in-one-wp-migration all-in-one-wp-migration-multisite-extension all-in-one-wp-migration-unlimited-extension ai1wp-backblazeall-in-one-wp-migration-url-extension all-in-one-wp-migration-b2-extension all-in-one-wp-migration-s3-extension
+#wp plugin activate all-in-one-wp-migration all-in-one-wp-migration-multisite-extension all-in-one-wp-migration-unlimited-extension ai1wp-backblazeall-in-one-wp-migration-url-extension all-in-one-wp-migration-b2-extension all-in-one-wp-migration-s3-extension
 wp plugin update all-in-one-wp-migration all-in-one-wp-migration-multisite-extension all-in-one-wp-migration-unlimited-extension all-in-one-wp-migration-url-extension all-in-one-wp-migration-b2-extension all-in-one-wp-migration-s3-extension
 wp ai1wm backup 
